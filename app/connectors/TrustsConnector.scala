94a2434cb8931225d914a2541ac535d34d240b2e
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-06-12 21:07:42 +0100

    Updated trusts connector to include setYearsReturns method.

--- app/connectors/TrustsConnector.scala
+++ app/connectors/TrustsConnector.scala
@@ -17,9 +17,9 @@
 package connectors
 
 import config.AppConfig
-import models.TrustDetails
+import models.{TrustDetails, YearsReturns}
 import uk.gov.hmrc.http.HttpReads.Implicits._
-import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpResponse}
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
@@ -27,10 +27,17 @@ import scala.concurrent.{ExecutionContext, Future}
 class TrustsConnector @Inject()(http: HttpClient, config: AppConfig) {
 
   private val trustsUrl: String = s"${config.trustsUrl}/trusts"
+  private val baseUrl: String = s"$trustsUrl/tax-liability"
 
   def getTrustDetails(identifier: String)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[TrustDetails] = {
     val url = s"$trustsUrl/trust-details/$identifier/untransformed"
     http.GET[TrustDetails](url)
+  }
+
+  def setYearsReturns(identifier: String, value: YearsReturns)
+                     (implicit hc: HeaderCarrier, ex: ExecutionContext): Future[HttpResponse] = {
+    val url = s"$baseUrl/$identifier/years-returns"
+    http.PUT[YearsReturns, HttpResponse](url, value)
   }
 
 }
