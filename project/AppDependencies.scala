7b1cc04d4c7a426659e7539e8cf0ae26e6b12113
Michael Sweeton <34985534+machinesleet@users.noreply.github.com>
2026-09-16 11:02:16 +0100

    DDCE-9184 bump

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,11 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "10.7.0"
-  private val mongoVersion     = "2.12.0"
+  private val mongoVersion     = "2.13.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.9.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.13.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoVersion,
     "uk.gov.hmrc"       %% "tax-year"                   % "6.0.0"
   )
